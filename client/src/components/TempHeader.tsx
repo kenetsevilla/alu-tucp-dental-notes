e6d217c434aa829e0d5f7fe0341e7a4fd247483a
Kenet Sevilla <sevillakenet104@gmail.com>
2025-12-18 11:37:35 +0800

    chore: remove accidental root-level package configuration

--- client/src/components/TempHeader.tsx
+++ client/src/components/TempHeader.tsx
@@ -1,5 +1,5 @@
 import './TempHeader.css';
-// import { FaBell } from 'react-icons/fa'; 
+import bellIcon from "../assets/bxs-bell.svg";
 import logoImage from '../assets/alu-logo.png'; 
 import profileImage from '../assets/senku.jpg'; 
 
@@ -10,18 +10,18 @@ function Header() {
         <div className="logo-section">
           <img src={logoImage} alt="ALU-TUCP Medical Center Logo" className="logo-image" />
         </div>
-
         <div className="user-section">
           <div className="notification-icon">
-        </div>
-        <div className="profile-info">
-          <span className="user-name">Dr. Senku Ishigami</span>
-          <div className="user-details">
-            <span className="user-id">40xxi</span>
-            <span className="user-role">icoxxi</span>
+            <img src={bellIcon} />
           </div>
-        </div>
-        <div className="profile-pic-container">
+          <div className="profile-info">
+            <span className="user-name">Dr. Senku Ishigami</span>
+            <div className="user-details">
+              <span className="user-id">40xxi</span>
+              <span className="user-role">icoxxi</span>
+            </div>
+          </div>
+          <div className="profile-pic-container">
             <img src={profileImage} alt="User Profile" className="profile-picture" />
           </div>
         </div>
